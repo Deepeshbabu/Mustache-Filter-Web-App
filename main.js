6f0c18834d7cc3d1664f5673c23c58f70027a696
Deepeshbabu <104300644+Deepeshbabu@users.noreply.github.com>
2022-11-23 19:39:56 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+function preload() 
+{
+
+}
+
+function setup() 
+{
+    can = createCanvas(300,300) ;
+    can.center() ;
+}
+
+function draw() 
+{
+
+}
+
+function tsk() 
+{
+    save('mfi.png') ;
+}
